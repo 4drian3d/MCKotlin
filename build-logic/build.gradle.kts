a9750a9bca34d6caa28c9012643075633f55a501
Adrian <adriangonzalesval@gmail.com>
2023-03-08 10:12:21 -0500

    fix: fixed gradle deprecation

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -11,7 +11,8 @@ repositories {
     gradlePluginPortal()
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
